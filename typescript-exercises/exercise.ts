567845b58c68f6e47bebf5c1693cc8773c76b09a
Angela Ngo <ngoangela6@gmail.com>
2025-03-31 18:51:24 -0400

    typescript part

--- typescript-exercises/exercise.ts
+++ typescript-exercises/exercise.ts
@@ -4,3 +4,30 @@
 
 // ONCE DONE, UNCOMMENT THE LINE BELOW AND RUN THE FILE
 // console.log(task);
+
+interface Task {
+    id: number;
+    title: string;
+    description: string;
+    dueDate: number;
+}
+
+enum Priority {
+    Low,
+    Medium,
+    High
+}
+
+interface TaskPriority extends Task {
+    priority: Priority;
+}
+
+// separated by commas, not semicolons like definitions
+const task: TaskPriority = {
+    id: 1,
+    title: "First",
+    description: "Do this first.",
+    dueDate: 20,
+    priority: Priority.Medium
+}
+
